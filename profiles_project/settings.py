4fc2cc9172a6be563e9a4419102b50ccb87ba8b8
Katkulator <matkokatic92@gmail.com>
2023-10-19 12:15:16 +0200

    Added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-dkci)vxguf1e9o1q(gz&soafft5ky)r5ihx(aza#gwr8pko=g^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-13-48-138-242.eu-north-1.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
